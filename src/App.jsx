ba8937d6f63c95800db655eaf6236b4c7a7df014
Nishant Kumar <johndoe@email.com>
2023-10-12 02:12:34 +0530

    added bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 import AccountsCard from './cards/AccountsCard';
 import CreditCard from './cards/CreditCard';
+import { CSVDataProvider } from './cards/CSVDataContext';
 
 import { Grid } from '@mui/material';
 
@@ -11,30 +12,31 @@ import { Grid } from '@mui/material';
 
 const App = () => {
     return (
-        <>
-            <CssBaseline />
-            <AppBar position='relative' color='primary'>
-                <Typography variant="h3" align='center' component="div" sx={{ flexGrow: 1 }}>
-                    Spendlyzer
-                </Typography>
-            </AppBar>
+        <CSVDataProvider>
+            <>
+                <CssBaseline />
+                <AppBar position='relative' color='primary'>
+                    <Typography variant="h3" align='center' component="div" sx={{ flexGrow: 1 }}>
+                        Spendlyzer
+                    </Typography>
+                </AppBar>
 
-            <Grid
-                container
-                justifyContent="center"
-                alignItems="flex-start"
-                spacing={2} // Add spacing here
-                sx={{ padding: 2 }} // Add padding using sx prop
-            >
-                <Grid item xs={12} sm={6} md={4}>
-                    <AccountsCard />
+                <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="flex-start"
+                    spacing={2}
+                    sx={{ padding: 2 }}
+                >
+                    <Grid item xs={12} sm={6} md={4}>
+                        <AccountsCard />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <CreditCard />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <CreditCard />
-                </Grid>
-
-            </Grid>
-        </>
+            </>
+        </CSVDataProvider>
     );
 }
 
