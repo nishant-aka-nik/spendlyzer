1b821b4b7dbc06146606fbc8346983dc82fa314e
Nishant Kumar <johndoe@email.com>
2023-10-14 19:22:52 +0530

    improvements

--- src/cards/AccountsCard.jsx
+++ src/cards/AccountsCard.jsx
@@ -6,98 +6,97 @@ import Divider from '@mui/material/Divider';
 import { useCSVData } from './CSVDataContext';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/joy/LinearProgress';
-import {getNextMonthName,getNextToNextMonthName} from './../utils/utils'
+import { getNextMonthName, getNextToNextMonthName } from './../utils/utils'
 
 
 export default function AccountsCard() {
-    const nextMonthName = getNextMonthName();
-    const nextToNextMonthName = getNextToNextMonthName();
+  const nextMonthName = getNextMonthName();
+  const nextToNextMonthName = getNextToNextMonthName();
 
-    const csvData = useCSVData();
+  const csvData = useCSVData();
 
-    const [balanceLinearProgress, setbalanceLinearProgress] = useState(0)
-    const [nextMonethSavingLinearProgress, setnextMonethSavingLinearProgress] = useState(0)
-    const [nextToNextMonthSavingLinearProgress, setnextToNextMonthSavingLinearProgress] = useState(0)
+  const [balanceLinearProgress, setbalanceLinearProgress] = useState(0)
+  const [nextMonethSavingLinearProgress, setnextMonethSavingLinearProgress] = useState(0)
+  const [nextToNextMonthSavingLinearProgress, setnextToNextMonthSavingLinearProgress] = useState(0)
+  const [disposableMoney, setdisposableMoney] = useState(0)
 
-    const disposableThisMonth =  csvData.thisMonth - csvData.totalSaving*0.2
+  useEffect(() => {
+    setbalanceLinearProgress((csvData.thisMonth / csvData.totalSaving) * 100);
+    setnextMonethSavingLinearProgress((csvData.nextMonth / csvData.totalSaving) * 100);
+    setnextToNextMonthSavingLinearProgress((csvData.nextNextMonth / csvData.totalSaving) * 100);
+    setdisposableMoney(getDisposableMoney(csvData))
+  }, [csvData]);
 
-    useEffect(() => {
-        setbalanceLinearProgress((csvData.thisMonth / csvData.totalSaving) * 100);
+  return (
+    <Card >
+      <CardContent orientation='horizontal' sx={{ paddingLeft: 2, paddingTop: 2, paddingBottom: 1 }}>
+        <Typography level="h2">Account</Typography>
+      </CardContent>
 
-    }, [csvData]);
+      <Divider variant="middle" />
 
+      <CardContent orientation='horizontal' sx={{ padding: 1 }}>
+        <CardContent orientation='vertical' sx={{ paddingLeft: 1 }}>
+          <Typography level="body-md">Balance</Typography>
+          <Typography level="h3">Rs. {csvData.thisMonth}</Typography>
+          <LinearProgressWithLabel value={balanceLinearProgress} />
+          <Typography level="body-sm">Disposable Rs. {disposableMoney}</Typography>
+        </CardContent>
+      </CardContent>
 
-    useEffect(() => {
-        setnextMonethSavingLinearProgress((csvData.nextMonth / csvData.totalSaving) * 100);
+      <Divider variant="middle" />
 
-    }, [csvData]);
+      <CardContent orientation='horizontal' sx={{ padding: 1 }}>
+        <CardContent orientation='vertical' sx={{ paddingLeft: 1 }}>
+          <Typography level="body-md">
+            {nextMonthName} Savings</Typography>
+          <Typography level="title-lg">Rs. {csvData.nextMonth}</Typography>
+          <LinearProgressWithLabel value={nextMonethSavingLinearProgress} />
+        </CardContent>
+      </CardContent>
 
-    useEffect(() => {
-        setnextToNextMonthSavingLinearProgress((csvData.nextNextMonth / csvData.totalSaving) * 100);
-    }, [csvData]);
+      <CardContent orientation='horizontal' sx={{ padding: 1 }}>
+        <CardContent orientation='vertical' sx={{ paddingLeft: 1 }}>
+          <Typography level="body-md">{nextToNextMonthName} Savings</Typography>
+          <Typography level="title-lg">Rs. {csvData.nextNextMonth}</Typography>
+          <LinearProgressWithLabel value={nextToNextMonthSavingLinearProgress} />
+        </CardContent>
+      </CardContent>
 
-    return (
-        <Card >
-            <CardContent orientation='horizontal' sx={{ paddingLeft:2, paddingTop:2, paddingBottom:1 }}>
-                <Typography level="h2">Account</Typography>
-            </CardContent>
+    </Card>
 
-            <Divider variant="middle" />
-
-            <CardContent orientation='horizontal' sx={{ padding: 1 }}>
-                <CardContent orientation='vertical' sx={{ paddingLeft: 1 }}>
-                    <Typography level="body-md">Balance</Typography>
-                    <Typography level="h3">Rs. {csvData.thisMonth}</Typography>
-                    <LinearProgressWithLabel value={balanceLinearProgress} />
-                    <Typography level="body-sm">Disposable Rs. {disposableThisMonth}</Typography>
-                </CardContent>
-            </CardContent>
-
-            <Divider variant="middle" />
-
-            <CardContent orientation='horizontal' sx={{ padding: 1 }}>
-                <CardContent orientation='vertical' sx={{ paddingLeft: 1 }}>
-                    <Typography level="body-md">
-                      {nextMonthName} Savings</Typography>
-                    <Typography level="title-lg">Rs. {csvData.nextMonth}</Typography>
-                    <LinearProgressWithLabel value={nextMonethSavingLinearProgress} />
-                </CardContent>
-            </CardContent>
-
-            <CardContent orientation='horizontal' sx={{ padding: 1 }}>
-                <CardContent orientation='vertical' sx={{ paddingLeft: 1 }}>
-                    <Typography level="body-md">{nextToNextMonthName} Savings</Typography>
-                    <Typography level="title-lg">Rs. {csvData.nextNextMonth}</Typography>
-                    <LinearProgressWithLabel value={nextToNextMonthSavingLinearProgress} />
-                </CardContent>
-            </CardContent>
-
-        </Card>
-
-    );
+  );
 }
 
 function LinearProgressWithLabel(props) {
-    const isLessThan20 = props.value < 20;
-    const barColor = isLessThan20 ? 'danger' : 'success'
-    const roundedValue = isNaN(props.value) ? 0 : Math.round(props.value);
+  const isLessThan20 = props.value < 20;
+  const barColor = isLessThan20 ? 'danger' : 'success'
+  const roundedValue = isNaN(props.value) ? 0 : Math.round(props.value);
 
-    return (
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Box sx={{ width: '100%', mr: 1 }}>
-          <LinearProgress
-            color={barColor}
-            determinate
-            size="lg"
-            variant="outlined"
-            value ={roundedValue}
-          />
-        </Box>
-        <Box sx={{ minWidth: 35 }}>
-          <Typography variant="body2" color="text.secondary">
-            {`${roundedValue}%`}
-          </Typography>
-        </Box>
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '100%', mr: 1 }}>
+        <LinearProgress
+          color={barColor}
+          determinate
+          size="lg"
+          variant="outlined"
+          value={roundedValue}
+        />
       </Box>
-    );
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">
+          {`${roundedValue}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+function getDisposableMoney(csvData) {
+  const disposable = csvData.thisMonth - (csvData.totalSaving * 0.2)
+  if (disposable < 0 || isNaN(disposable)){
+    return 0;
   }
+  return disposable
+}
