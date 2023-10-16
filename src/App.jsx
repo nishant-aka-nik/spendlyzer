34948fb466261d27a9203bbcffd19e224f1ea6d9
Nishant Kumar <johndoe@email.com>
2023-10-16 15:10:05 +0530

    design

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,37 @@ import CreditCard from './cards/CreditCard';
 import { CSVDataProvider } from './cards/CSVDataContext';
 import { Grid } from '@mui/material';
 
+const styles = {
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: 'linear-gradient(0deg, rgba(174,238,206,1) 0%, rgba(148,233,203,0) 100%)',
+    },
+};
+
 const App = () => {
     return (
         <CSVDataProvider>
-            <Grid
-                container
-                direction="column"
-                justifyContent="flex-start"
-                alignItems="stretch"
-
-                spacing={2}
-                sx={{
-                    paddingLeft: 1,
-                    paddingRight: 1,
-                    paddingTop: 1,
-                }}
-            >
-                <Grid item xs={12} sm={6} md={4} lg={4}>
-                    <UserCard />
+            <div style={styles.container}>
+                <Grid
+                    container
+                    direction="column"
+                    rowSpacing={1}
+                    xs={12} sm={6} md={5} lg={4}
+                    padding={1}
+                >
+                    <Grid item>
+                        <UserCard />
+                    </Grid>
+                    <Grid item>
+                        <AccountsCard />
+                    </Grid>
+                    <Grid item>
+                        <CreditCard />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <AccountsCard />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <CreditCard />
-                </Grid>
-            </Grid>
+            </div>
         </CSVDataProvider>
     );
 }
